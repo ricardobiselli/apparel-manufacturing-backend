4fcb56e8b106c6aa70113255f20f64b3709fa845
ricardobiselli <ricardobiselli@gmail.com>
2026-05-29 00:23:55 -0300

    Synchronize order status with machine session lifecycle

--- Domain/Models/MachineEvent.cs
+++ Domain/Models/MachineEvent.cs
@@ -5,8 +5,9 @@ public abstract class MachineEvent
 {
     [Key]
     public int MachineEventId { get; set; }
+    public int? MachineSessionId { get; set; }
+    public MachineSession MachineSession { get; set; }
     public int? MachineId { get; set; }
     public Machine Machine { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
-
 }
--- Domain/Models/OperationLog.cs
+++ Domain/Models/OperationLog.cs
@@ -2,15 +2,6 @@
 {
     public class OperationLog : MachineEvent
     {
-        public int MachineSessionId { get; set; }
-        public MachineSession MachineSession { get; set; }
-        public OperationLog() { }
-
-        public OperationLog(int machineSessionId)
-        {
-            MachineSessionId = machineSessionId;
-        }
     }
-
 }
 
